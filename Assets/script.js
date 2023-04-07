d5482323f184325dfa17d7c94e9f84a4e39cd4b1
JJ <jenniferj1796@gmail.com>
2023-04-06 20:41:23 -0400

    corrected a code line in JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,24 @@
 // Assignment Code
-var options= []
+var options= [];
 var passwordLength = 8;
 
-var lowercase= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
-var uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",]
-var numbers= ["1","2","3","4","5","6","7","8","9"]
+var lowercase= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+var numbers= ["1","2","3","4","5","6","7","8","9"];
 var characters= ["?","#","@","$","%","!","&","^","*",".","+","/",];
 
 
 var generateBtn = document.querySelector("#generate");
 
-//this makes the red button that says generate password start the function in line 60 which will begin by calling on line 17 by clicking it
+//this makes the red button that says generate password start the function in line 61 which will begin by calling on line 17 by clicking it
 generateBtn.addEventListener("click", writePassword);
 
 
 function popUpAlert(){
-  var number 
+  var number=0
+
+  // If the user does not input a number(in integer format) between 8-128 it will not let them continue to the rest of the ifs and it will return false for function on line 61 after the alert
   passwordLength= parseInt(prompt("What would you like your password length to be? 8-128 characters"));
-// If the user does not input a number(in integer format) between 8-128 it will not let them continue to the rest of the ifs and it will return false for function on line 60 after the alert
    if(isNaN(passwordLength)|| passwordLength<8 || passwordLength >128){
      alert("Password length is not met. It needs to be between 8 to 128 charaters");
      return false;
@@ -38,8 +39,8 @@ function popUpAlert(){
     options = options.concat(characters);
     number++
    } 
-//this will count the number of answers true above and if all where clicked false then it will return false for function on line 60 since var number will remain zero
-// this is to force the user to atleast pick 1 character
+   //this will count the number of answers true above and if all where clicked false then it will return false for function on line 61 since var number will remain zero
+   // this is to force the user to atleast pick 1 character
    if (number==0){ 
      return false;   
    }
@@ -48,16 +49,16 @@ function popUpAlert(){
 }
 
 function generatePassword() {
- console.log("This button works")
- var randomPassword = "";
- for(var i=0; i< passwordLength;i++) {
-   var randomCharacters = Math.floor(Math.random()* options.length);
-   randomPassword = randomPassword + options[randomCharacters];
+  console.log("This button works")
+  var randomPassword = "";
+  for(var i=0; i< passwordLength;i++) {
+    var randomCharacters = Math.floor(Math.random()* options.length);
+    randomPassword = randomPassword + options[randomCharacters];
+  }
+  return randomPassword;
  }
- return randomPassword;
-}
 
-function writePassword() {
+ function writePassword() {
   var metCritera = popUpAlert();
   
   if(metCritera) {
@@ -70,3 +71,4 @@ function writePassword() {
    alert("Click button again")
   }
 }
+
